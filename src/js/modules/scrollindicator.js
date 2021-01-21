5a7e09e414ef84831215c059695fd531f840e746
Sergey <segeyxdesign@gmail.com>
2021-01-21 23:17:27 +0100

    fix bugs: header, sidebar

---
+++ src/js/modules/scrollindicator.js
@@ -0,0 +1,8 @@
+window.addEventListener('scroll', () => {
+
+    const windowScroll = document.body.scrollTop || document.documentElement.scrollTop,
+        height = document.documentElement.scrollHeight - document.documentElement.clientHeight,
+        scroll = (windowScroll / height) * 100;
+    document.querySelector('.progress-bar').style.width = scroll + '%';
+    console.log(scroll);
+})
